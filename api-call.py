4cc8b16985c7fd0f27b03332b0595ea36c7e6201
Andreas <35858164+a-doering@users.noreply.github.com>
2021-04-17 16:51:50 +0200

    Add streamlit basic visualization

--- api-call.py
+++ api-call.py
@@ -1,23 +1,31 @@
 import os
 import openai
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
-prompt_path = "prompt.txt"
-with open(prompt_path, "r") as f:
-  prompt = f.read()
-print(prompt)
-
 openai.api_key = os.environ.get("OPENAI_API_KEY")
-response = openai.Completion.create(
-  engine="davinci",
-  prompt=prompt,
-  temperature=0,
-  max_tokens=188,
-  top_p=1,
-  frequency_penalty=0.29,
-  presence_penalty=0
-)
 
-print(100*'#')
-print(response)
+def main():
+  """"Request a text from user, query it to API and display"""
+  st.title("GPT3 Makeathon")
+  st.header("HGF Challengers: Papent Applications")
+  prompt = st.text_area("Enter your text")
+  response = request(prompt)
+  st.write(response.choices[0].text)
+
+def request(prompt):
+  """Request to Openais GPT3 API """
+  response = openai.Completion.create(
+    engine="davinci",
+    prompt=prompt,
+    temperature=0,
+    max_tokens=188,
+    top_p=1,
+    frequency_penalty=0.29,
+    presence_penalty=0
+  )
+  return response
+
+if __name__ == "__main__":
+  main()
