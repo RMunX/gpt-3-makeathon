daa6edf42715ec967d19e10b73056ce5fce402b7
Andreas <35858164+a-doering@users.noreply.github.com>
2021-04-17 20:54:20 +0200

    Add more choices of tasks with their configs

--- api-call.py
+++ api-call.py
@@ -2,30 +2,70 @@ import os
 import openai
 from dotenv import load_dotenv
 import streamlit as st
+#import logging
+import time
+import json
 
 load_dotenv()
 openai.api_key = os.environ.get("OPENAI_API_KEY")
+# logging.basicConfig(
+#   filename="example.log", 
+#   encoding="utf-8", 
+#   level=logging.INFO, 
+#   format="%(asctime)s — %(levelname)s — %(name)s — %(message)s",
+#   datefmt="%Y/%m/%d %H:%M:%S"
+# )
 
 def main():
   """"Request a text from user, query it to API and display"""
   st.title("GPT3 Makeathon")
   st.header("HGF Challengers: Papent Applications")
-  prompt = st.text_area("Enter your text")
-  response = request(prompt)
+
+  options = {
+    "Summarize prior claims": "summary",
+    "Create a problem description from the inventors problem description and a prior art example": "problem_description",
+    "Create a solution description based on the inventors solution description and a prior art example": "solution_description"
+    }
+  choice = st.selectbox("What would you like to do?", tuple(options.keys()))
+  with open("conf/conf.json") as f:
+    conf = json.load(f)
+  parameters = conf[options[choice]]
+
+  # Create prompt from different inputs and commands
+  if options[choice] == "summary":
+    prompt = st.text_area("Enter your text")
+    command = "\n\n\nOne-sentence summary:\n\n"
+    prompt = f'{prompt}{command}'
+  elif options[choice] == "problem_description":
+    problem = st.text_area("Enter the problem")
+    prior_art = st.text_area("Enter prior art")
+    command = "\nDescribe the problem mentioned in the first text:\n"
+    prompt = f'{problem}"""{prior_art}"""'
+  elif options[choice] == "solution_description":
+    solution = st.text_area("Enter the solution")
+    prior_art = st.text_area("Enter prior art")
+    command = "\nDescribe the solution proposed in text 1 like it was in text 2:\n"
+    prompt = f'{solution}"""{prior_art}"""'
+
+  # Generate response
+  response = request(prompt, parameters)
   st.write(response.choices[0].text)
 
-def request(prompt):
+def request(prompt, param):
   """Request to Openais GPT3 API """
+  #TODO: find out how to make stop work for empty stop sequences
   response = openai.Completion.create(
-    engine="davinci",
+    engine=param["engine"],
     prompt=prompt,
-    temperature=0,
-    max_tokens=188,
-    top_p=1,
-    frequency_penalty=0.29,
-    presence_penalty=0
+    temperature=param["temperature"],
+    max_tokens=param["max_tokens"],
+    top_p=param["top_p"],
+    frequency_penalty=param["frequency_penalty"],
+    presence_penalty=param["presence_penalty"],
+    stop=param["stop"]
   )
   return response
 
 if __name__ == "__main__":
   main()
+  
--- readme.md
+++ readme.md
@@ -2,8 +2,17 @@
 
 ## Installation
 Requirements: python >= 3.6, tested with 3.6.9.
-```
+```sh
 virtualenv -p python3 .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+```
+
+## Secrets
+Add your API key to the .env file in the main directory with the parentheses like:
+`OPENAI_API_KEY = "your_secret_numbers"`
+
+## Run
+```sh
+streamlit run api-call.py
 ```
